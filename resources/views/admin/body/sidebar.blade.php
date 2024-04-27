dc8f54216d4faa9aae2fd341d2916c3e22f88a90
suryaherdiyanto <suryaherdiyanto@gmail.com>
2024-04-27 11:19:59 +0800

    sidebar

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -20,39 +20,6 @@
             </a>
         </li>
 
-        <li>
-            <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class="bx bx-category"></i>
-                </div>
-                <div class="menu-title">Manage Teams </div>
-            </a>
-            <ul>
-                @if(Auth::user()->can('team.all'))
-                <li> <a href="{{ route('all.team') }}"><i class='bx bx-radio-circle'></i>All Team</a>
-                </li>
-                @endif
-                @if(Auth::user()->can('team.add'))
-                <li> <a href="{{ route('add.team') }}"><i class='bx bx-radio-circle'></i>Add Team</a>
-                </li>
-                @endif
-            </ul>
-        </li>
-
-        <li>
-            <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class="bx bx-category"></i>
-                </div>
-                <div class="menu-title">Manage Book Area </div>
-            </a>
-            <ul>
-                @if(Auth::user()->can('update.bookarea'))
-                <li> <a href="{{ route('book.area') }}"><i class='bx bx-radio-circle'></i>Update BookArea </a>
-                </li>
-                @endif
-
-            </ul>
-        </li>
-
         <li class="menu-label">Booking Manage </li>
 
         <li>
