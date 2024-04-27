3fdd624a6b3c66180e1fd466348751235f553e30
suryaherdiyanto <suryaherdiyanto@gmail.com>
2024-04-27 11:22:54 +0800

    fixed add admin

--- resources/views/backend/pages/admin/add_admin.blade.php
+++ resources/views/backend/pages/admin/add_admin.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.admin_dashboard')
-@section('admin') 
+@section('admin')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
 <div class="page-content">
@@ -15,72 +15,59 @@
 							</ol>
 						</nav>
 					</div>
-					 
+
 				</div>
 				<!--end breadcrumb-->
 				<div class="container">
 					<div class="main-body">
 						<div class="row">
-						 
+
     <div class="col-lg-12">
-        
+
         <div class="card">
 
             <div class="card-body p-4">
-                
+
        <form  class="row g-3" action="{{ route('store.admin') }}" method="post" enctype="multipart/form-data">
          @csrf
 
-                 
-    
+
+
     <div class="col-md-6">
         <label for="input1" class="form-label">Admin User Name </label>
         <input type="text" name="name" class="form-control"   >
-         
+
     </div>
 
     <div class="col-md-6">
         <label for="input1" class="form-label">Admin User Email </label>
         <input type="email" name="email" class="form-control"   >
-         
+
     </div>
 
     <div class="col-md-6">
         <label for="input1" class="form-label">Admin User Phone </label>
         <input type="text" name="phone" class="form-control"   >
-         
+
     </div>
     <div class="col-md-6">
         <label for="input1" class="form-label">Admin User Address </label>
         <input type="text" name="address" class="form-control"   >
-         
+
     </div>
 
 
     <div class="col-md-6">
         <label for="input1" class="form-label">Admin Password </label>
         <input type="password" name="password" class="form-control"   >
-         
+
     </div>
 
-    <div class="col-md-6">
-        <label for="input1" class="form-label">Role Name </label>
-        <select name="roles" class="form-select mb-3" aria-label="Default select example">
-            <option selected="">Select Role </option>
-            @foreach ($roles as $role)
-            <option value="{{ $role->id }}">{{ $role->name }} </option> 
-            @endforeach
-            
-        </select>
-         
-    </div>
-    
- 
-                 
+
                     <div class="col-md-12">
                         <div class="d-md-flex d-grid align-items-center gap-3">
                             <button type="submit" class="btn btn-primary px-4">Save Changes </button>
-                            
+
                         </div>
                     </div>
                 </form>
@@ -95,6 +82,6 @@
 			</div>
 
 
-            
+
 
 @endsection
