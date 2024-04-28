ed7018fd4060e5ee82f405fd30365012379b85b0
suryaherdiyanto <suryaherdiyanto@gmail.com>
2024-04-28 09:50:07 +0800

    adjust sidebar

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -40,17 +40,13 @@
             <a class="has-arrow" href="javascript:;">
                 <div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
                 </div>
-                <div class="menu-title">Manage RoomList</div>
+                <div class="menu-title">Manage Room List</div>
             </a>
             <ul>
                 <li> <a href="{{ route('view.room') }}"><i class='bx bx-radio-circle'></i>Rooms</a>
                 </li>
-                <li> <a href="{{ route('view.room.list') }}"><i class='bx bx-radio-circle'></i>Room List</a>
+                <li> <a href="{{ route('add.room') }}"><i class='bx bx-plus'></i>Add Room</a>
                 </li>
-
-                <li> <a href="{{ route('room.type.list') }}"><i class='bx bx-radio-circle'></i>Room Type List </a>
-                </li>
-
             </ul>
         </li>
 
